def86957a2773e5aefd5cee3ab49bfd0a98c87b7
Karthick Naveen <karthickn03C@gmail.com>
2026-07-30 09:46:35 +0530

    fix: safe subprojects compileSdkVersion override and disable incremental compilation

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -5,17 +5,6 @@ allprojects {
     }
 }
 
-subprojects {
-    subprojects.forEach { subproject ->
-        subproject.afterEvaluate {
-            if (subproject.plugins.hasPlugin("com.android.library") || subproject.plugins.hasPlugin("com.android.application")) {
-                val android = subproject.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
-                android?.compileSdkVersion(36)
-            }
-        }
-    }
-}
-
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -26,17 +15,26 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
-    project.evaluationDependsOn(":app")
+    fun applyCompileSdk(p: Project) {
+        val android = p.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        android?.compileSdkVersion(36)
+    }
+    if (project.state.executed) {
+        applyCompileSdk(project)
+    } else {
+        project.afterEvaluate {
+            applyCompileSdk(project)
+        }
+    }
 }
 
 subprojects {
-    plugins.withId("com.android.library") {
-        val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
-        android?.compileSdkVersion(36)
-    }
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
