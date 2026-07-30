e06d44c581c783178e009023b77b3f8029807a8c
Karthick Naveen <karthickn03C@gmail.com>
2026-07-30 08:43:19 +0530

    chore: clean build configuration for mobile android

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -19,15 +19,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            val android = project.property("android") as? com.android.build.gradle.BaseExtension
-            android?.compileSdkVersion(36)
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
